bac71a2601462b2afb06459c17cccbb13d90f87a
sikwan <yysskk99999@naver.com>
2024-05-13 12:14:21 +0900

    init

---
+++ WatchApp_EX/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WatchApp_EX
+//
+//  Created by 양시관 on 5/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ WatchApp_EX/WatchApp_EXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp_EXApp.swift
+//  WatchApp_EX
+//
+//  Created by 양시관 on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_EXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
